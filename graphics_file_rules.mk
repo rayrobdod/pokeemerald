1365a95b46231a5635024b94aa84a89674654096
Raymond Dodge <git@rayrobdod.name>
2024-05-29 07:59:42 -0400

    Change city_zoom_text.png's width to match logical width

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -667,6 +667,9 @@ $(PKNAVGFXDIR)/device_outline.4bpp: %.4bpp: %.png
 $(PKNAVGFXDIR)/match_call/ui.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -num_tiles 13 -Wnum_tiles
 
+$(PKNAVGFXDIR)/region_map/city_zoom_text.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -num_tiles 64 -Wnum_tiles
+
 $(POKEDEXGFXDIR)/region_map.8bpp: %.8bpp: %.png
 	$(GFX) $< $@ -num_tiles 232 -Wnum_tiles
 
